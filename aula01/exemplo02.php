25533dc8a376a63827366a6acbe544a2a01e166c
Evellyn dos Santos Furtado <161509297+evellynfurtad0@users.noreply.github.com>
2026-02-10 17:27:01 -0300

    Add files via upload

---
+++ aula01/exemplo02.php
@@ -0,0 +1,12 @@
+<?php
+    $nota = 7;
+    if ($nota >= 6) {
+      echo "Aprovado!";
+    } else {
+      echo "Reprovado!";
+    }
+    echo "<br>";
+    for ($i = 1; $i <= 5; $i++) {
+      echo "Número: " . $i . "<br>";
+    }
+?>
